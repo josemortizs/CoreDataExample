c6bec403dbb7889d59e896cabb5e9fdd9ce2abe0
José Manuel Ortiz Sánchez <josemortizs@gmail.com>
2020-02-18 19:47:32 +0100

    Initial Commit

---
+++ DatabaseExample/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DatabaseExample
+//
+//  Created by Jose Manuel Ortiz Sanchez on 18/02/2020.
+//  Copyright © 2020 Ortiz Sánchez DEV. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
